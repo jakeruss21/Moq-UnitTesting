e87b57670225f0fd6879ea82699b5bca72eb226e
Jacob Russell <jacob.russell82@outlook.com>
2018-10-04 10:18:35 -0400

    creates and saves TestMethods for the EssentialsTools Tester

---
+++ EssentialTools.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTools.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialTools.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("adb35b1f-6340-4645-b4b3-6fd93c1a71ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssentialTools.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
+
+namespace EssentialTools.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IDiscountHelper getTestObject()
+        {
+            return new MinnumumDiscountHelper();
+        }
+
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            //arrange
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+
+            //act
+            var discountedTotal = target.ApplyDiscount(total);
+
+            //assert
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+        
+    }
+}
