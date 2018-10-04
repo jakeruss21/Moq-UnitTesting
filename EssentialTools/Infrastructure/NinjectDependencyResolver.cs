de6f4fcaeb442c224a252d2f56b0c5d323f28df1
Jacob Russell <jacob.russell82@outlook.com>
2018-10-04 09:27:10 -0400

    adds dependency chain in the NinjectDependencyResolver for the Discount and ValueCalculator

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -31,6 +31,7 @@ namespace EssentialTools.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>();
         }
     }
 
